5c1d6ff8aecdf11f9e7e4e0eb0daa3529b9c136a
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-09-30 06:03:27 +0300

    lab complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
   get '/students/:id', to: 'students#show'
+  get 
 end
